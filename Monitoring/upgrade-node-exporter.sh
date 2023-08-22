005cd72f799b4125ee8db2078d5a790b1d96ad02
hossein1376 <hosseinyazdani1376@gmail.com>
2023-08-22 11:33:23 +0330

    Upgrade node_exporter script

---
+++ Monitoring/upgrade-node-exporter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script will upgrade node-exporter on a linux machine.
+# Hossein Yazdani, hosseinyazdani1376@gmail.com
+# August 2023
+
+# node-exporter version
+version=1.6.1
+
+# check if running as root, or exit
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+# exit in case of error
+set -e
+
+# download and extract the files
+wget https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz
+tar xvf node_exporter-${version}.linux-amd64.tar.gz
+cd node_exporter-${version}.linux-amd64 || { echo "Failed to change directory."; exit 1; }
+
+# remove old binaries
+rm /usr/bin/node_exporter
+
+# move to path and set the ownership
+mv node_exporter /usr/bin/
+chown node_exporter:node_exporter /usr/bin/node_exporter
+
+systemctl restart node-exporter
+systemctl status node-exporter
+
+# cleanup
+cd ..
+rm node_exporter-${version}.linux-amd64.tar.gz
+rm -rf node_exporter-${version}.linux-amd64
